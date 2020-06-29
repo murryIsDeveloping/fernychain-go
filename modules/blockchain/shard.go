afec257d19102103055af4687bce00dca7aedb9d
murryIsDeveloping <marcusbrincat1@gmail.com>
2020-06-29 21:45:14 +1000

    shard  map

--- modules/blockchain/shard.go
+++ modules/blockchain/shard.go
@@ -1,9 +1,23 @@
 package blockchain
 
+const shardSize = 10
+
 // Shard represents a small chunk of the blockchain
 type Shard struct {
 	id          string
 	lastHash    string
 	blocks      []*Block
 	prevShardID string
+}
+
+func CreateShardMap() *map[string][]int {
+	return &make(map[string][]int)
+}
+
+func RemovePeerFromShardMap(m *map[string][]int, peer string) {
+	delete(m, peer)
+}
+
+func AddPeerToShardMap(m *map[string][]int, peer string, shards []int) {
+	m[peer] = shards
 }
